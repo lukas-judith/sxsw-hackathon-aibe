ae6885313d7b4c5b3fcd11d4686644e13807fae7
Lukas Judith <lukasjudith12@gmail.com>
2024-10-18 19:02:57 +1100

    Switch to llama 3.1 70B

--- src/extract_responsibilities.py
+++ src/extract_responsibilities.py
@@ -20,31 +20,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# client = Groq(
-#     api_key=os.environ.get("GROQ_API_KEY"),
-# )
 
-# chat_completion = client.chat.completions.create(
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": "Explain the importance of fast language models",
-#         }
-#     ],
-#     model="llama3-8b-8192",
-# )
-
-# # import pdb
-
-# # pdb.set_trace()
-
-# print(chat_completion.choices[0].message.content)
-
-
-# import dspy
-
-
-lm = dspy.LM("groq/llama3-8b-8192")
+lm = dspy.LM("groq/llama-3.1-70b-versatile")
+#  llama3-8b-8192")
 
 dspy.configure(lm=lm)
 
